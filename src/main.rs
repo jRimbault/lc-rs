7210c54cd26ce447b746266ea5bbf2d620abe0e3
jRimbault <jacques.rimbault@gmail.com>
2020-09-17 16:46:47 +0200

    add global stats

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
@@ -8,12 +9,22 @@ use structopt::StructOpt;
 struct Args {
     #[structopt(parse(from_os_str))]
     path: PathBuf,
+    #[structopt(short, long)]
+    verbose: bool,
 }
 
 fn main() {
     let args = Args::from_args();
-    let entries: HashMap<PathBuf, FileStats> = walker(&args.path, anaylize_entry);
-    println!("{:#?}", entries);
+    let entries: HashMap<PathBuf, Stats> = walker(&args.path, anaylize_entry);
+    if args.verbose {
+        for (filename, stats) in entries.iter() {
+            println!("{}", filename.display());
+            println!("{}", textwrap::indent(&stats.to_string(), "  "));
+        }
+    }
+    let lines: Vec<usize> = entries.into_iter().flat_map(|(_, f)| f.lines).collect();
+    let stats = Stats::from(lines);
+    println!("{}", stats);
 }
 
 fn walker<P: AsRef<Path>, A, I, E, B>(path: &P, analyzer: A) -> B
@@ -41,7 +52,7 @@ where
     receiver.iter().filter_map(Result::ok).collect()
 }
 
-fn anaylize_entry(entry: ignore::DirEntry) -> anyhow::Result<(PathBuf, FileStats)> {
+fn anaylize_entry(entry: ignore::DirEntry) -> anyhow::Result<(PathBuf, Stats)> {
     let path = entry.into_path();
     let file = BufReader::new(File::open(&path)?);
     let line_lengths: Vec<usize> = file
@@ -49,22 +60,46 @@ fn anaylize_entry(entry: ignore::DirEntry) -> anyhow::Result<(PathBuf, FileStats
         .filter_map(Result::ok)
         .map(|l| l.chars().count())
         .collect();
-    Ok((path, FileStats::from(line_lengths.as_ref())))
+    Ok((path, Stats::from(line_lengths)))
 }
 
 #[derive(Debug, Default)]
-struct FileStats {
+struct Stats {
+    lines: Vec<usize>,
     max: usize,
+    min: usize,
     mean: f64,
     median: f64,
+    standard_deviation: f64,
 }
 
-impl From<&[usize]> for FileStats {
-    fn from(list: &[usize]) -> Self {
-        FileStats {
-            max: *list.iter().max().unwrap_or(&0),
-            mean: stats::mean(list.iter().copied()),
-            median: stats::median(list.iter().copied()).unwrap_or(0.),
+impl From<Vec<usize>> for Stats {
+    fn from(list: Vec<usize>) -> Self {
+        let flist: Vec<_> = list.iter().copied().map(|i| i as _).collect();
+        Stats {
+            max: list.iter().copied().max().unwrap_or(0),
+            min: list
+                .iter()
+                .copied()
+                .filter(|i| ![0, 1].contains(i))
+                .min()
+                .unwrap_or(0),
+            mean: statistical::mean(&flist),
+            median: statistical::median(&flist),
+            standard_deviation: statistical::standard_deviation(&flist, None),
+            lines: list,
         }
+    }
+}
+
+impl fmt::Display for Stats {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(
+            f,
+            "Length (mean ± σ):  {:>4.1} ±  {:>4.1}",
+            self.mean, self.standard_deviation
+        )?;
+        write!(f, "Range (min … max):  {:>4} …  {:>4}", self.min, self.max)?;
+        write!(f, " (excluding lengths < 2)")
     }
 }
