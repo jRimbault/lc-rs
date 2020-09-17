fe1b8ecace6cff750c5247f9f9743c7fa437d8a9
jRimbault <jacques.rimbault@gmail.com>
2020-09-17 15:06:45 +0200

    start prototyping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Args {
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::from_args();
+    let (sender, receiver) = std::sync::mpsc::channel();
+    ignore::WalkBuilder::new(&args.path)
+        .build_parallel()
+        .run(move || {
+            let sender = sender.clone();
+            Box::new(move |result| {
+                if result.is_err() {
+                    return ignore::WalkState::Continue;
+                }
+                let entry = result.unwrap();
+                sender.send(entry).unwrap();
+                ignore::WalkState::Continue
+            })
+        });
+    let entries: Vec<ignore::DirEntry> = receiver.iter().collect();
+    println!("{:?}", entries);
 }
